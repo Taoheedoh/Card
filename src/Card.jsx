2bb83cf5dc8d74e2a7169a90e57bfee6aed2aa27
Taoheedoh <taoheedoh@gmail.com>
2024-05-21 00:07:03 +0100

    Add: modify the styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,8 +4,8 @@ function Card(){
     return(
         <div className='card'>
             <img className='card-image' src={profile} alt="profile pics" />
-            <h2>Taoheed Hamzat</h2>
-            <p>I am a Front-End Developer and an entrepreneur</p>
+            <h2 className='card-tittle'>Taoheed Hamzat</h2>
+            <p className='card-text'>I am a Front-End Developer and an entrepreneur</p>
         </div>
     )
 }
